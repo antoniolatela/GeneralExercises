99e35c3bc064e20cd7240aec6b18051b3c45aa4e
Antonio Latela <alatel01@dcs.bbk.ac.uk>
2017-10-16 12:16:55 +0100

    yac

--- Session4-5.py
+++ Session4-5.py
@@ -1,10 +1,31 @@
-#List
-l =  [1,2,3,4,5]
-l[4] = 'hi there!'
-l.append(1974)
-print (l[-2], l[-1])
 
-l.append(l)
-l[1] = 123
-print (l[1], l[6][6][6][1])
 
+class One():
+    def __init__(self):
+        print ("Hi!")
+
+    def foo(self):
+        print ("I'm foo")
+        print("bar")
+
+    def listz (self):
+        #List
+        print("Test Lists")
+        l =  [1,2,3,4,5]
+        l[4] = 'hi there!'
+        l.append(1974)
+        print (l[-2], l[-1])
+        l.append(l)
+        l[1] = 123
+        print (l[1], l[6][6][6][1])
+        return l
+
+    def getRangeCount(self):
+        for i in range (0, len(self.listz())):
+            print (i,self.listz()[i])
+
+#One().foo()
+#One().listz()
+
+one = One()
+one.getRangeCount()
